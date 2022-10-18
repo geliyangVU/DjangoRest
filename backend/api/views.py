8976780f4169ab50fed18c7cf21a5fa7eadea3b4
Geli Yang <gyang5@villanova.edu>
2022-10-18 10:29:43 -0400

    practice echo get request
    practice echo get request

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
+import json
+
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"hi there this is your django api response"})
+    body = request.body
+    data={}
+    try:
+        data = json.loads(body)  # string of Json data -> Python Dictionary
+    except:
+        pass
+    print(data)
+
+    data['headers'] = dict(request.headers)
+    print(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,9 +6,11 @@ import requests
 endpoint = "http://localhost:8000/api"
 
 
-get_response = requests.get(endpoint)
-print(get_response.text)
-print(get_response.json())#print the response in JSON format
+get_response = requests.get(endpoint, params={"abc": 123}, json = {"query": "hello world"})
+# print(get_response.text)
+# print(get_response.json())#print the response in JSON format
+# print(get_response.json()['message'])#print the response.message
+
 print(get_response.status_code)# print the status code for debugging purpose
 
 
