20c7a04b810829400a14e2a8729760b956d402c8
Geli Yang <gyang5@villanova.edu>
2022-10-18 11:41:08 -0400

    migrate to django rest framework
    Used @api_view with a list of http methods allowed.
    Imported and Used Response Object from rest_framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
 import json
 from products.models import Product
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['id','title','price'])
+    return Response(data)
