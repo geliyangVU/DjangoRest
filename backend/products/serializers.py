846deb76eee4be3c9c1a21775af290bbf90fbe83
Geli Yang <gyang5@villanova.edu>
2022-10-21 11:38:51 -0400

    implement customized method with serializer
    implement customized method/field my_discount with Django rest framework serializer.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,11 @@
 import imp
+from pickletools import read_long4
 from rest_framework import serializers
 
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta :
         model = Product
         fields = [
@@ -11,5 +13,8 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'get_discount'
+            'my_discount'
         ]
+
+    def get_my_discount(self, obj):
+        return obj.get_discount()
