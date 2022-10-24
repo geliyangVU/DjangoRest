6aa7450819675961bc9f23c9616156abe06b9fa0
Geli Yang <gyang5@villanova.edu>
2022-10-24 15:18:51 -0400

    tested product/pk endpoint
    tested product/pk endpoint by using detail.py

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<>', views.ProductDetailAPIView.as_view())
+    path('<int:pk>/', views.ProductDetailAPIView.as_view())
 ]
 
